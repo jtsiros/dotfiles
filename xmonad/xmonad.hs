97569e8a046683686e69ba1bdcd61200d6f8da74
Jon Tsiros <jon.tsiros@gmail.com>
2020-07-17 21:49:20 -0700

    doom emacs!

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -102,7 +102,7 @@ myBrowser = "firefox"               -- Sets firefox as browser for tree select
 -- myBrowser = myTerminal ++ " -e lynx " -- Sets lynx as browser for tree select
 
 myEditor :: String
-myEditor = "nvim"                   -- Sets emacs as editor for tree select
+myEditor = "emacs"                   -- Sets emacs as editor for tree select
 
 myBorderWidth :: Dimension
 myBorderWidth = 2          -- Sets border width for windows

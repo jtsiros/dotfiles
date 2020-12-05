1c063919630de8134fc32184a6dbdd09a29c1b78
Jon Tsiros <jon.tsiros@gmail.com>
2020-12-04 16:17:15 -0800

    add new packages, add org-mode enhancements, and keybinds

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -23,38 +23,33 @@
 (setq doom-font (font-spec :family "BlexMono Nerd Font" :size 15)
       doom-unicode-font (font-spec :family "BlexMono Nerd Font" :size 15)
       doom-variable-pitch-font (font-spec :family "BlexMono Nerd Font" :size 15)
-      projectile-project-search-path '("~/dev/code/"))
+      projectile-project-search-path '("~/dev/code/")
+      mac-command-modifier 'meta
+      default-directory "~"
+      org-ellipsis " ▾ "
+      org-bullets-bullet-list '("·")
+      org-tags-column -80
+      org-agenda-files (ignore-errors (directory-files +org-dir t "\\.org$" t))
+      org-log-done 'time
+      org-refile-targets (quote ((nil :maxlevel . 1)))
+      org-capture-templates '(("x" "Note" entry
+                          (file+olp+datetree "journal.org")
+                          "**** [ ] %U %?" :prepend t :kill-buffer t)
+                         ("t" "Task" entry
+                          (file+headline "tasks.org" "Inbox")
+                          "* [ ] %?\n%i" :prepend t :kill-buffer t))
+      +org-capture-todo-file "tasks.org")
 
-;; sets the mac option modifier key. It doesn't seem to register without it.
-(setq mac-option-modifier 'meta)
-
+(map! :ne "M-/" #'comment-or-uncomment-region)
+(map! :ne "SPC / r" #'deadgrep)
+(map! :ne "SPC n b" #'org-brain-visualize)
+(map! :ne "SPC k" #'save-buffer)
+;;
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dark+)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/notes/")
-
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.

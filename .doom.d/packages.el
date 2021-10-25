d2bfe05445f3d90a4353d6a5103c35d8ca3e0fb4
Jon Tsiros <tsiros@cockroachlabs.com>
2021-10-25 15:12:09 -0700

    add prezto configuration
    Signed-off-by: Jon Tsiros <tsiros@cockroachlabs.com>

--- .doom.d/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-(package! org-fancy-priorities)
-(package! org-super-agenda)
-(package! deadgrep)
-(package! sql-indent)
-(package! org-brain)
-(package! ascii-art-to-unicode)

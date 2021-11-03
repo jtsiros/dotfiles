93c45fa1dfd9d48a38b82ece0831bc9689135843
Jon Tsiros <tsiros@cockroachlabs.com>
2021-11-03 14:04:41 -0700

    updated lvim and kitty configs
    Signed-off-by: Jon Tsiros <tsiros@cockroachlabs.com>

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,9 +1,13 @@
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.format_on_save = true
 lvim.lint_on_save = true
-lvim.colorscheme = "codemonkey"
+lvim.colorscheme = "github_dark"
+lvim.builtin.lualine.options.theme = "github_dark"
+
+-- vim options
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -31,7 +35,10 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup.view.width = 45
+lvim.builtin.nvimtree.icons.git.unstaged = "λ"
+lvim.builtin.nvimtree.icons.git.untracked = "★"
+
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
@@ -50,12 +57,12 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- end
 
 -- set a formatter if you want to override the default lsp one (if it exists)
-lvim.lang.go.formatters = {
-  {
-    exe = "goimports",
-    args = {}
-  }
-}
+-- lvim.lang.go.formatters = {
+--   {
+--     exe = "goimports",
+--     args = {}
+--   }
+-- }
 
 -- set an additional linter
 -- lvim.lang.python.linters = {
@@ -69,6 +76,7 @@ lvim.lang.go.formatters = {
 lvim.plugins = {
     {"lunarvim/colorschemes"},
     {"oberblastmeister/neuron.nvim"},
+    {"projekt0n/github-nvim-theme"},
     {
         "iamcco/markdown-preview.nvim",
         run = "cd app && npm install",
@@ -76,10 +84,41 @@ lvim.plugins = {
         config = function()
           vim.g.mkdp_auto_start = 1
         end,
-    }
+    },
+    {
+        'kristijanhusak/orgmode.nvim',
+        branch = 'tree-sitter',
+        config = function()
+                require('orgmode').setup{}
+        end
+    },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = {'src/parser.c', 'src/scanner.cc'},
+  },
+  filetype = 'org',
+}
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+require('orgmode').setup({
+  org_agenda_files = {'~/Google Drive/My Drive/org/*', '~/orgs/**/*'},
+  org_default_notes_file = '~/Google Drive/My Drive/org/notes.org',
+})

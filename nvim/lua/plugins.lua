71d821fae064ff4cebee33449d59821e7847477f
Jon Tsiros <jon.tsiros@gmail.com>
2022-06-29 11:29:29 -0700

    use comment package in line with lunarvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,113 +1,118 @@
 require("packer").startup(function(use)
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- =====================
-  -- UI STUFF
-  -- =====================
-  -- Top buffer/tab line
-  use("akinsho/nvim-bufferline.lua")
-  -- Gruvbox theme
-  use("morhetz/gruvbox")
-  -- Dracula
-  use({ "darker-dracula/vim", as = "dracula" })
-  -- use("~/dev/darker-dracula/vim")
-  use("mangeshrex/uwu.vim")
-  -- github theme
-  use("projekt0n/github-nvim-theme")
-  -- Git in the gutter
-  use("lewis6991/gitsigns.nvim")
-  -- dev-icons
-  use("kyazdani42/nvim-web-devicons")
-  -- UI stuff (mainly used for lsp overrides)
-  use("RishabhRD/popfix")
-  use("RishabhRD/nvim-lsputils")
-  -- Trouble
-  use("folke/trouble.nvim")
-  -- Colorizer
-  use("norcalli/nvim-colorizer.lua")
-  -- indent lines
-  use("lukas-reineke/indent-blankline.nvim")
-  -- statusline
-  use({ "famiu/feline.nvim", branch = "develop" })
+	-- =====================
+	-- UI STUFF
+	-- =====================
+	-- Top buffer/tab line
+	use("akinsho/nvim-bufferline.lua")
+	-- Gruvbox theme
+	use("morhetz/gruvbox")
+	-- Dracula
+	use({ "darker-dracula/vim", as = "dracula" })
+	-- use("~/dev/darker-dracula/vim")
+	use("mangeshrex/uwu.vim")
+	-- github theme
+	use("projekt0n/github-nvim-theme")
+	-- Git in the gutter
+	use("lewis6991/gitsigns.nvim")
+	-- dev-icons
+	use("kyazdani42/nvim-web-devicons")
+	-- UI stuff (mainly used for lsp overrides)
+	use("RishabhRD/popfix")
+	use("RishabhRD/nvim-lsputils")
+	-- Trouble
+	use("folke/trouble.nvim")
+	-- Colorizer
+	use("norcalli/nvim-colorizer.lua")
+	-- indent lines
+	use("lukas-reineke/indent-blankline.nvim")
+	-- statusline
+	use({ "famiu/feline.nvim", branch = "develop" })
 
-  use("tpope/vim-scriptease")
-  -- symbols outline for viewing symbols in a tree like view
-  use("simrat39/symbols-outline.nvim")
-  -- =====================
-  -- TELESCOPE
-  -- =====================
-  use({
-    "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/plenary.nvim" }, { "nvim-lua/popup.nvim" } },
-  })
-  use({
-    "romgrk/fzy-lua-native",
-    requires = { { "nvim-telescope/telescope.nvim" } },
-  })
-  use({
-    "nvim-telescope/telescope-fzy-native.nvim",
-    requires = { { "nvim-telescope/telescope.nvim" } },
-  })
+	use("tpope/vim-scriptease")
+	-- symbols outline for viewing symbols in a tree like view
+	use("simrat39/symbols-outline.nvim")
+	-- =====================
+	-- TELESCOPE
+	-- =====================
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" }, { "nvim-lua/popup.nvim" } },
+	})
+	use({
+		"romgrk/fzy-lua-native",
+		requires = { { "nvim-telescope/telescope.nvim" } },
+	})
+	use({
+		"nvim-telescope/telescope-fzy-native.nvim",
+		requires = { { "nvim-telescope/telescope.nvim" } },
+	})
 
-  -- =====================
-  -- Coding stuff
-  -- =====================
-  -- note/organization stuff
-  use("nvim-lua/plenary.nvim")
-  -- lsp stuff
-  use("neovim/nvim-lspconfig")
+	-- =====================
+	-- Coding stuff
+	-- =====================
+	-- note/organization stuff
+	use("nvim-lua/plenary.nvim")
+	-- lsp stuff
+	use("neovim/nvim-lspconfig")
 
-  -- completion plugin
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-vsnip")
-  use("hrsh7th/cmp-emoji")
+	-- completion plugin
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/cmp-emoji")
 
-  use("L3MON4D3/LuaSnip")
+	use("L3MON4D3/LuaSnip")
 
-  use("williamboman/nvim-lsp-installer")
+	use("williamboman/nvim-lsp-installer")
 
-  -- tree-sitter
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use("nvim-treesitter/playground")
-  use("windwp/nvim-ts-autotag")
-  use("JoosepAlviste/nvim-ts-context-commentstring")
-  -- Lua dev
-  use("folke/lua-dev.nvim")
-  use("ckipp01/stylua-nvim")
-  -- snippets
-  use("hrsh7th/vim-vsnip")
-  -- Ranger
-  use({ "kevinhwang91/rnvimr", run = ":make sync" })
-  -- Comment stuff out
-  use("tpope/vim-commentary")
-  -- Set root directory properly
-  use("airblade/vim-rooter")
-  -- Show a popup with signature info
-  use("ray-x/lsp_signature.nvim")
-  -- idk
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("jose-elias-alvarez/nvim-lsp-ts-utils")
-  -- rust
-  use("simrat39/rust-tools.nvim")
+	-- tree-sitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("windwp/nvim-ts-autotag")
+	-- Lua dev
+	use("folke/lua-dev.nvim")
+	use("ckipp01/stylua-nvim")
+	-- snippets
+	use("hrsh7th/vim-vsnip")
+	-- Ranger
+	use({ "kevinhwang91/rnvimr", run = ":make sync" })
+	-- Comments
+	use({
+		"numToStr/Comment.nvim",
+		event = "BufRead",
+		config = function()
+			require("utils.comment").setup()
+		end
+	})
+	-- Set root directory properly
+	use("airblade/vim-rooter")
+	-- Show a popup with signature info
+	use("ray-x/lsp_signature.nvim")
+	-- idk
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jose-elias-alvarez/nvim-lsp-ts-utils")
+	-- rust
+	use("simrat39/rust-tools.nvim")
 
-  -- =====================
-  -- OTHERS
-  -- =====================
-  -- Pairs ()
-  use("windwp/nvim-autopairs")
-  -- Startify
-  use("mhinz/vim-startify")
-  -- Which key
-  -- use("liuchengxu/vim-which-key")
-  use("folke/which-key.nvim")
-  -- git
-  use("tpope/vim-fugitive")
-  use("antoinemadec/FixCursorHold.nvim")
-  -- file trees
-  use("kyazdani42/nvim-tree.lua")
-  -- tpope
-  use("tpope/vim-surround")
+	-- =====================
+	-- OTHERS
+	-- =====================
+	-- Pairs ()
+	use("windwp/nvim-autopairs")
+	-- Startify
+	use("mhinz/vim-startify")
+	-- Which key
+	-- use("liuchengxu/vim-which-key")
+	use("folke/which-key.nvim")
+	-- git
+	use("tpope/vim-fugitive")
+	use("antoinemadec/FixCursorHold.nvim")
+	-- file trees
+	use("kyazdani42/nvim-tree.lua")
+	-- tpope
+	use("tpope/vim-surround")
 end)

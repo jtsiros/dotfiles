657bb7a267c5f0db918b84bb38ba767b4c43aee1
Jon Tsiros <jon.tsiros@gmail.com>
2022-06-29 11:29:29 -0700

    use comment package in line with lunarvim

--- nvim/lua/utils/comment.lua
+++ nvim/lua/utils/comment.lua
@@ -1,11 +1,61 @@
-local function comment()
-  require("ts_context_commentstring.internal").update_commentstring()
-  local mode = vim.fn.mode()
-  if mode == "n" then
-    vim.fn.feedkeys("gcc")
-  elseif mode == "v" or mode == "V" then
-    vim.fn.feedkeys("gc")
-  end
+local M = {}
+
+function M.config()
+	return {
+		active = true,
+		on_config_done = nil,
+		---Add a space b/w comment and the line
+		---@type boolean
+		padding = true,
+
+		---Lines to be ignored while comment/uncomment.
+		---Could be a regex string or a function that returns a regex string.
+		---Example: Use '^$' to ignore empty lines
+		---@type string|function
+		ignore = "^$",
+
+		---Whether to create basic (operator-pending) and extra mappings for NORMAL/VISUAL mode
+		---@type table
+		mappings = {
+			---operator-pending mapping
+			---Includes `gcc`, `gcb`, `gc[count]{motion}` and `gb[count]{motion}`
+			basic = true,
+			---extended mapping
+			---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+			extra = false,
+		},
+
+		---LHS of line and block comment toggle mapping in NORMAL/VISUAL mode
+		---@type table
+		toggler = {
+			---line-comment toggle
+			line = "gcc",
+			---block-comment toggle
+			block = "gbc",
+		},
+
+		---LHS of line and block comment operator-mode mapping in NORMAL/VISUAL mode
+		---@type table
+		opleader = {
+			---line-comment opfunc mapping
+			line = "gc",
+			---block-comment opfunc mapping
+			block = "gb",
+		},
+
+		---Pre-hook, called before commenting the line
+		---@type function|nil
+		pre_hook = pre_hook,
+
+		---Post-hook, called after commenting is done
+		---@type function|nil
+		post_hook = nil,
+	}
 end
 
-return comment
+function M.setup()
+	local nvim_comment = require("Comment")
+	nvim_comment.setup(M.config())
+end
+
+return M
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -69,6 +69,15 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["Comment.nvim"] = {
+    after_files = { "/home/jtsiros/.local/share/nvim/site/pack/packer/opt/Comment.nvim/after/plugin/Comment.lua" },
+    config = { "\27LJ\2\2;\0\0\2\0\3\0\0066\0\0\0'\1\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\18utils.comment\frequire\0" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/home/jtsiros/.local/share/nvim/site/pack/packer/opt/Comment.nvim",
+    url = "https://github.com/numToStr/Comment.nvim"
+  },
   ["FixCursorHold.nvim"] = {
     loaded = true,
     path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/FixCursorHold.nvim",
@@ -204,11 +213,6 @@ _G.packer_plugins = {
     path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
-  ["nvim-ts-context-commentstring"] = {
-    loaded = true,
-    path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring",
-    url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
-  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
@@ -279,11 +283,6 @@ _G.packer_plugins = {
     path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/uwu.vim",
     url = "https://github.com/mangeshrex/uwu.vim"
   },
-  ["vim-commentary"] = {
-    loaded = true,
-    path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/vim-commentary",
-    url = "https://github.com/tpope/vim-commentary"
-  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/home/jtsiros/.local/share/nvim/site/pack/packer/start/vim-fugitive",
@@ -322,6 +321,13 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+vim.cmd [[augroup packer_load_aucmds]]
+vim.cmd [[au!]]
+  -- Event lazy-loads
+time([[Defining lazy-load event autocommands]], true)
+vim.cmd [[au BufRead * ++once lua require("packer.load")({'Comment.nvim'}, { event = "BufRead *" }, _G.packer_plugins)]]
+time([[Defining lazy-load event autocommands]], false)
+vim.cmd("augroup END")
 if should_profile then save_profiles() end
 
 end)

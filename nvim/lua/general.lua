510792c93b13ec6a480d8d2f6c9900a34574a371
Jon Tsiros <jon.tsiros@gmail.com>
2022-08-31 22:21:20 -0500

    rust support

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -33,6 +33,26 @@ vim.opt.termguicolors = true
 vim.opt.textwidth = 90
 vim.opt.colorcolumn = "90"
 
-vim.opt.completeopt = "menuone,noinsert,noselect"
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
 
-vim.opt.shortmess:append("c")
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Treesitter folding 
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'

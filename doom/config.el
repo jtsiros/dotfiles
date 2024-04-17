ba6b5c0ac684c088b023b009373267ce50909516
Jon Tsiros <jon@brightblock.ai>
2024-04-17 16:57:15 -0500

    fix author email

--- doom/config.el
+++ doom/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Jon Tsiros"
-       user-mail-address "jontsiros@scratchpad.com")
+       user-mail-address "jon@brightblock.ai")
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)

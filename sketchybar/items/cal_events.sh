55fe8f347d8c89a0baec73b6600dab65ac7e2ead
Jon Tsiros <jon@brightblock.ai>
2024-08-16 11:41:41 -0500

    use calendar-events cli instead of script

--- sketchybar/items/cal_events.sh
+++ sketchybar/items/cal_events.sh
@@ -2,5 +2,5 @@
 
 sketchybar --add item calendar_events right \
            --set calendar_events icon=􀧞  \
-                          update_freq=30 \
+                          update_freq=60 \
                           script="$PLUGIN_DIR/cal_events.sh"

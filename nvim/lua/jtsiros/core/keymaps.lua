119248d4034bbef91627f20313bf4669d2ed5076
Jon Tsiros <jtsiros@strongtie.com>
2025-08-12 14:41:08 -0500

    feat: improve Neovim UI and navigation
    - Add custom tabline showing only filenames with proper colors
    - Fix YAML indentation to use 4 spaces (removed from 2-space list)
    - Add directory management keymaps under <leader>cd prefix:
      - <leader>cdc: change directory
      - <leader>cdf: change to current file's directory
      - <leader>cdp: show current directory
    - Update which-key descriptions for new directory commands
    - Enhance tabline colors to match vague colorscheme

--- nvim/lua/jtsiros/core/keymaps.lua
+++ nvim/lua/jtsiros/core/keymaps.lua
@@ -1,23 +1,110 @@
-vim.g.mapleader = " "
+-- Core keymaps and shortcuts
+-- These are global keymaps that don't depend on plugins
 
-local keymap = vim.keymap -- for conciseness
+-- Note: Leader key is set in options.lua
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+-- =============================================================================
+-- General Mappings
+-- =============================================================================
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- Better escape
+map("i", "jk", "<ESC>", vim.tbl_extend("force", opts, { desc = "Exit insert mode" }))
+map("i", "kj", "<ESC>", vim.tbl_extend("force", opts, { desc = "Exit insert mode" }))
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- Clear search highlights
+map("n", "<leader>nh", "<cmd>nohlsearch<CR>", vim.tbl_extend("force", opts, { desc = "Clear search highlights" }))
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- Better indenting
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+
+-- Move text up and down
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Stay in indent mode
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+
+-- Keep cursor centered during navigation
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
+
+-- =============================================================================
+-- Number Operations
+-- =============================================================================
+
+map("n", "<leader>+", "<C-a>", vim.tbl_extend("force", opts, { desc = "Increment number" }))
+map("n", "<leader>-", "<C-x>", vim.tbl_extend("force", opts, { desc = "Decrement number" }))
+map("v", "<leader>+", "<C-a>", vim.tbl_extend("force", opts, { desc = "Increment numbers" }))
+map("v", "<leader>-", "<C-x>", vim.tbl_extend("force", opts, { desc = "Decrement numbers" }))
+
+-- =============================================================================
+-- Window Management
+-- =============================================================================
+
+-- Splits
+map("n", "<leader>sv", "<C-w>v", vim.tbl_extend("force", opts, { desc = "Split window vertically" }))
+map("n", "<leader>sh", "<C-w>s", vim.tbl_extend("force", opts, { desc = "Split window horizontally" }))
+map("n", "<leader>se", "<C-w>=", vim.tbl_extend("force", opts, { desc = "Make splits equal size" }))
+map("n", "<leader>sx", "<cmd>close<CR>", vim.tbl_extend("force", opts, { desc = "Close current split" }))
+
+-- Window navigation (enhanced with tmux navigator)
+map("n", "<C-h>", "<C-w>h", vim.tbl_extend("force", opts, { desc = "Move to left window" }))
+map("n", "<C-j>", "<C-w>j", vim.tbl_extend("force", opts, { desc = "Move to bottom window" }))
+map("n", "<C-k>", "<C-w>k", vim.tbl_extend("force", opts, { desc = "Move to top window" }))
+map("n", "<C-l>", "<C-w>l", vim.tbl_extend("force", opts, { desc = "Move to right window" }))
+
+-- Leader + w navigation (alternative to Ctrl keys)
+map("n", "<leader>wh", "<C-w>h", vim.tbl_extend("force", opts, { desc = "Move to left window" }))
+map("n", "<leader>wj", "<C-w>j", vim.tbl_extend("force", opts, { desc = "Move to bottom window" }))
+map("n", "<leader>wk", "<C-w>k", vim.tbl_extend("force", opts, { desc = "Move to top window" }))
+map("n", "<leader>wl", "<C-w>l", vim.tbl_extend("force", opts, { desc = "Move to right window" }))
+
+-- Resize windows
+map("n", "<C-Up>", "<cmd>resize +2<CR>", opts)
+map("n", "<C-Down>", "<cmd>resize -2<CR>", opts)
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", opts)
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", opts)
+
+-- =============================================================================
+-- Tab Management
+-- =============================================================================
+
+map("n", "<leader>to", "<cmd>tabnew<CR>", vim.tbl_extend("force", opts, { desc = "Open new tab" }))
+map("n", "<leader>tx", "<cmd>tabclose<CR>", vim.tbl_extend("force", opts, { desc = "Close current tab" }))
+map("n", "<leader>tn", "<cmd>tabnext<CR>", vim.tbl_extend("force", opts, { desc = "Go to next tab" }))
+map("n", "<leader>tp", "<cmd>tabprevious<CR>", vim.tbl_extend("force", opts, { desc = "Go to previous tab" }))
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", vim.tbl_extend("force", opts, { desc = "Open current buffer in new tab" }))
+
+-- Navigate tabs with alt
+map("n", "<A-1>", "<cmd>1tabnext<CR>", opts)
+map("n", "<A-2>", "<cmd>2tabnext<CR>", opts)
+map("n", "<A-3>", "<cmd>3tabnext<CR>", opts)
+map("n", "<A-4>", "<cmd>4tabnext<CR>", opts)
+map("n", "<A-5>", "<cmd>5tabnext<CR>", opts)
+
+-- =============================================================================
+-- Directory Management
+-- =============================================================================
+
+-- Change directory group
+map("n", "<leader>cdc", ":cd ", { desc = "Change directory" })
+map("n", "<leader>cdf", "<cmd>cd %:h<CR>", vim.tbl_extend("force", opts, { desc = "Change to current file's directory" }))
+map("n", "<leader>cdp", "<cmd>pwd<CR>", vim.tbl_extend("force", opts, { desc = "Show current directory" }))
+
+-- =============================================================================
+-- Buffer Management
+-- =============================================================================
+
+-- Navigate buffers
+map("n", "<S-l>", "<cmd>bnext<CR>", opts)
+map("n", "<S-h>", "<cmd>bprevious<CR>", opts)
+
+-- Close buffers
+map("n", "<leader>bx", "<cmd>bd<CR>", vim.tbl_extend("force", opts, { desc = "Close current buffer" }))

26beec8c7e09d60d41b883692d648ad465a34c72
Jon Tsiros <jtsiros@strongtie.com>
2025-09-11 09:04:16 -0500

    remove doom emacs configuration
    Remove old Doom Emacs configuration files as they've been replaced
    with a custom Elpaca-based Emacs setup. This includes:
    
    - Core configuration files (config.el, init.el, packages.el, custom.el)
    - LSP configurations (golang.el, lsp.el, odin.el)
    - Package configurations (json.el, odin.el)
    - Theme files (doom-asteroid-theme.el)
    - Organization configuration (org.el)
    - General configuration (general.el)

--- doom/config/packages/json.el
+++
@@ -1,6 +0,0 @@
-;;; $DOOMDIR/config/packages/json.el -*- lexical-binding: t; -*-
-
-;; formatters
-(after! json-mode
-  (set-formatter! 'jq "jq ." :modes '(json-mode))
-  (add-hook 'json-mode-hook #'format-all-mode))
--- doom/config/packages/odin.el
+++
@@ -1,6 +0,0 @@
-;;; $DOOMDIR/config/packages/odin.el -*- lexical-binding: t; -*-
-
-;; Odin language config
-(use-package! odin-mode
-  :mode ("\\.odin\\'" . odin-mode)
-  :hook (odin-mode . lsp))

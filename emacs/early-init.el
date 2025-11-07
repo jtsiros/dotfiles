14ff97c05f541fe0c81247c68e630b025968f48f
Jon Tsiros <jon@brightblock.ai>
2025-11-06 23:44:41 -0600

    Add native compilation support and byte-compile helper
    - Enable native compilation for Emacs 28+ (faster than byte-compilation)
    - Silence native-comp warnings for cleaner startup
    - Add jt/byte-compile-config helper function for manual compilation
    - Config.org is automatically byte-compiled by org-babel-load-file

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,5 +1,13 @@
 (setq package-enable-at-startup nil)
 
+;; Native compilation settings (Emacs 28+)
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings
+  (setq native-comp-async-report-warnings-errors nil)
+  ;; Set native-comp directory
+  (setq native-comp-eln-load-path
+        (list (expand-file-name "eln-cache/" user-emacs-directory))))
+
 ;; Optimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
